4512c3247f6a014cfc2ebfd9c1350c738abdb861
Samuel Haddad <Sam@Samplusplus.com>
2022-09-18 21:45:30 -0400

    Update MainActivity.kt
    Unit 1 - Dice Roller, Button Clicks and Classes. To Dynamically update an activity

--- Unit 1 - Kotlin Basics/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit 1 - Kotlin Basics/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result on screen
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,8 +15,23 @@ class MainActivity : AppCompatActivity() {
 
         var rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener(){
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        rollButton.setOnClickListener() {
+            rollDice()
         }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result
+     */
+    private fun rollDice() {
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = Dice(6).roll().toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
