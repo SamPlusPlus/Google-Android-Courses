8f6c2a27db7537bbb2dd06878669985a6cd28d9a
Samuel Haddad <Sam@Samplusplus.com>
2022-09-20 21:54:06 -0400

    Implement Tip Calculator Functionality
    Implement Tip Calculator

--- Unit-2-User-Input/TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Unit-2-User-Input/TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tipResult.text = ""
+        binding.calculateButton.setOnClickListener {
+
+            calculateTip()
+        }
+
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        //Selected Tip
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        //Calculate the tip
+        var tip = tipPercentage * cost
+
+        //Round up if switched on
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
